ccc308b7a804fe6ad51c4271c9cc9b185ebec167
Toe, Knee <leletonyli.13944@yahoo.com.au>
2019-02-04 19:40:36 +1000

    Added more groups and types

--- src/SharedTypes.ts
+++ src/SharedTypes.ts
@@ -1,11 +1,12 @@
+
 export type CallbackMethods = {
-    onPartSelect(name: string, path: string, group: string): void,
+    onPartSelect(name: string, path: string, group: string): void;
 }
 
 export type OwoindiePartData = {
     partName: string;
     partPath: string;
-    groupName: string,
+    groupName: string;
 }
 
 export type RenderingPanelProps = {
@@ -13,12 +14,20 @@ export type RenderingPanelProps = {
 }
 
 export type GroupProps = {
-    groupName: string,
+    groupName: string;
+    tabColour: string;
 };
 
 export enum GroupIndexMapping {
+    BACKGROUND,
     BASE,
     EYEBROWS,
     EYES,
+    EYEBALLS,
+    BLUSHES,
+    FACE_ACCESSORIES,
+    MOUTH,
+    HAIR_ACCESSORIES,
+    EARS,
     HAIR,
 }
--- src/containers/SelectionPanel.tsx
+++ src/containers/SelectionPanel.tsx
@@ -7,11 +7,38 @@ export default class SelectionPanel extends React.Component<CallbackMethods & Re
         super(props);
     }
 
+
+
     public render(): React.ReactNode {
         return (
             <section className="panel">
-                <PartGrouping groupName="Eyes" onPartSelect={this.props.onPartSelect}/>
-                <PartGrouping groupName="Eyebrows" onPartSelect={this.props.onPartSelect}/>
+                <PartGrouping groupName="Eyes" onPartSelect={this.props.onPartSelect}
+                    selectedParts={this.props.selectedParts} tabColour="#fb6363"
+                />
+                <PartGrouping groupName="Eyeballs" onPartSelect={this.props.onPartSelect}
+                    selectedParts={this.props.selectedParts} tabColour="#fb6363"
+                />
+                <PartGrouping groupName="Eyebrows" onPartSelect={this.props.onPartSelect}
+                    selectedParts={this.props.selectedParts} tabColour="#fb6363"
+                />
+                <PartGrouping groupName="Blushes" onPartSelect={this.props.onPartSelect}
+                    selectedParts={this.props.selectedParts} tabColour="#fb6363"
+                />
+                <PartGrouping groupName="Face_accessories" onPartSelect={this.props.onPartSelect}
+                    selectedParts={this.props.selectedParts} tabColour="#fb6363"
+                />
+                <PartGrouping groupName="Mouth" onPartSelect={this.props.onPartSelect}
+                    selectedParts={this.props.selectedParts} tabColour="#fb6363"
+                />
+                <PartGrouping groupName="Ears" onPartSelect={this.props.onPartSelect}
+                    selectedParts={this.props.selectedParts} tabColour="#fb6363"
+                />
+                <PartGrouping groupName="Hair_accessories" onPartSelect={this.props.onPartSelect}
+                    selectedParts={this.props.selectedParts} tabColour="#fb6363"
+                />
+                <PartGrouping groupName="Background" onPartSelect={this.props.onPartSelect}
+                    selectedParts={this.props.selectedParts} tabColour="#fb6363"
+                />
             </section>
         );
     }
